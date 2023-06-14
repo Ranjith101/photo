f117c97ba1a43bb1db9c6cc6f32fa12468a06d94
Ranjith101 <89287908+Ranjith101@users.noreply.github.com>
2023-06-14 18:44:17 +0530

    changes

--- src/app.test.js
+++ src/app.test.js
@@ -6,14 +6,11 @@ describe('PhotoUpload', () => {
   test('should upload and display the image', () => {
     render(<PhotoUpload />);
 
-    // Select the file input element
     const fileInput = screen.getByLabelText('Upload Photo:');
 
-    // Create a sample image file
     const imageBlob = new Blob(['image content'], { type: 'image/jpeg' });
     const imageFile = new File([imageBlob], '1.jpg', { type: 'image/jpeg' });
 
-    // Mock FileReader to handle readAsDataURL
     const mockFileReader = {
       onload: jest.fn(),
       readAsDataURL: jest.fn().mockImplementation(function () {
@@ -23,22 +20,16 @@ describe('PhotoUpload', () => {
     };
     global.FileReader = jest.fn(() => mockFileReader);
 
-    // Simulate the file upload event
     fireEvent.change(fileInput, { target: { files: [imageFile] } });
 
-    // Verify that the image is displayed
     const uploadedImage = screen.getByAltText('Uploaded');
     expect(uploadedImage).toBeInTheDocument();
     expect(uploadedImage).toHaveAttribute('src', 'mocked result');
 
-    // Verify that the upload button is present
     const uploadButton = screen.getByRole('button', { name: 'Upload' });
     expect(uploadButton).toBeInTheDocument();
 
-    // Simulate the form submission event
     fireEvent.click(uploadButton);
 
-    // Perform additional assertions or dispatch actions to check the behavior after form submission
-    // For example, you can dispatch an action and verify that the image data is stored in Redux or sent to the server
   });
 });
